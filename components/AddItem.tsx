ba0dd965f90237833f3b0955dfe90c66225697a9
Ben S. Anderson <ben.anderson@assurant.com>
2019-02-19 16:09:05 -0500

    added rating

--- components/AddItem.tsx
+++ components/AddItem.tsx
@@ -1,12 +1,30 @@
 import React, {Component} from 'react';
-import { StyleSheet, View} from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
+import { Rating } from 'react-native-ratings';
 
 interface Props {}
-export default class AddItem extends Component<Props> {
+interface State {
+  descriptions: Array<String>,
+  severity: number,
+}
+
+export default class AddItem extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      severity: 2,
+      descriptions: ['Best', 'No Pain', 'Some Pain', 'Moderate Pain', 'A Lot of Pain', 'Worst'],
+    };
+  }
   render() {
     return (
       <View style={styles.container}>
-
+        <Text style={styles.ratingLabel}>{this.state.descriptions[this.state.severity]}</Text>
+        <Rating
+          type="star"
+          imageSize={40}
+          onFinishRating={(rating)=> this.setState({severity: rating})}
+        />
       </View>
     );
   }
@@ -21,4 +39,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5CC88',
   },
+  ratingLabel: {
+    fontSize: 20
+  }
 });
